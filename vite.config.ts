36a4ca095e8d80d383dc386e4b004140d0c465ac
humanerror9000 <human.error9000@gmail.com>
2026-04-15 10:40:31 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3DAudioViz/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
